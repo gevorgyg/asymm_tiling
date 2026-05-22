3adc0966245556de038bb658e0a6e644e5cb828d
aregmk6 <areg631@gmail.com>
2026-05-22 22:03:09 +0300

    make the sparse matrix implementation use a list so we can create huge matrices

--- lin-alg/matrix.cpp
+++ lin-alg/matrix.cpp
@@ -1,36 +1,27 @@
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <random>
-#include <vector>
 
 template <typename T>
-class Matrix
+class RandMatrix
 {
   public:
-    enum mat_type {
-        sparse,
-        dense,
-    };
-
-    Matrix(size_t n) : elems_(n * n), width_(n), height_(n), nr_elems_(n * n)
+    RandMatrix(size_t n)
+        : elems_(new T[n * n]), width_(n), height_(n), nr_elems_(n * n)
     {
+        fill();
     }
 
-    Matrix(size_t n, size_t m)
-        : elems_(n * m), width_(n), height_(m), nr_elems_(n * m)
+    RandMatrix(size_t n, size_t m)
+        : elems_(new T[n * m]), width_(n), height_(m), nr_elems_(n * m)
     {
+        fill();
     }
 
-    Matrix(size_t n, mat_type t)
-        : elems_(n * n), width_(n), height_(n), nr_elems_(n * n)
+    ~RandMatrix()
     {
-        fill(t);
-    }
-
-    Matrix(size_t n, size_t m, mat_type t)
-        : elems_(n * m), width_(n), height_(m), nr_elems_(n * m)
-    {
-        fill(t);
+        delete[] elems_;
     }
 
     const T& getElem(size_t row, size_t col) const
@@ -43,12 +34,12 @@ class Matrix
         return (elems_[row * width_ + col]);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& m)
+    friend std::ostream& operator<<(std::ostream& os, const RandMatrix<T>& m)
     {
         size_t counter = 0;
-        for (const auto& elem : m.elems_) {
-            os << elem;
-            if (counter % m.width_ == 0) {
+        for (int i = 0; i < m.nr_elems_; ++i) {
+            os << m.elems_[i];
+            if (counter != 0 && counter % m.width_ == 0) {
                 os << std::endl;
             } else {
                 os << ' ';
@@ -60,49 +51,123 @@ class Matrix
     }
 
   private:
-    std::vector<T> elems_;
+    T* elems_;
     size_t width_;
     size_t height_;
     size_t nr_elems_;
 
-    void fill(mat_type t)
+    void fill()
+    {
+        std::mt19937 prng_eng_{std::random_device()()};
+
+        for (int i = 0; i < nr_elems_; ++i) {
+            elems_[i] = prng_eng_();
+        }
+    }
+};
+
+template <typename T>
+class SparseMatrix
+{
+  public:
+    struct element {
+        T value;
+        size_t row;
+        size_t col;
+    };
+
+    SparseMatrix(size_t n) : width_(n), height_(n), nr_elems_(n * n)
+    {
+        fill();
+    }
+
+    SparseMatrix(size_t n, size_t m) : width_(n), height_(m), nr_elems_(n * m)
+    {
+        fill();
+    }
+
+    const T& getElem(size_t row, size_t col) const
+    {
+        element e = elems_[row * width_ + col];
+        if (e.row == row && e.col == col) {
+            return e.value;
+        } else {
+            return 0;
+        }
+    }
+
+    T& getElem(size_t row, size_t col)
+    {
+        element e = elems_[row * width_ + col];
+        if (e.row == row && e.col == col) {
+            return e.value;
+        } else {
+            return 0;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const SparseMatrix<T>& m)
+    {
+        size_t counter = 0;
+        auto it        = m.elems_.begin();
+        size_t row;
+        size_t col;
+        for (int i = 0; i < m.nr_elems_; ++i) {
+            row = i / m.width_;
+            col = i % m.width_;
+
+            if (row == it->row && col == it->col) {
+                os << it->value;
+                ++it;
+            } else {
+                os << 0;
+            }
+            if (counter != 0 && counter % m.width_ == 0) {
+                os << std::endl;
+            } else {
+                os << ", ";
+            }
+            ++counter;
+        }
+
+        return os;
+    }
+
+  private:
+    std::list<element> elems_;
+    size_t width_;
+    size_t height_;
+    size_t nr_elems_;
+
+    void fill()
     {
         // generates numbers between 0 to 2^32
         std::mt19937 prng_eng_{std::random_device()()};
 
-        switch (t) {
-        case sparse: {
-            std::mt19937 to_put_eng_{std::random_device()()};
+        std::mt19937 to_put_eng_{std::random_device()()};
 
-            size_t treshold = nr_elems_ >> 5;
-            if (!nr_elems_) {
-                nr_elems_ = 1;
-            }
+        size_t treshold = nr_elems_ >> 8;
+        if (!nr_elems_) {
+            nr_elems_ = 1;
+        }
 
-            for (int i = 0; i < nr_elems_; ++i) {
-                int to_put = to_put_eng_() % nr_elems_;
-                if (to_put < treshold) { // put in a number
-                    elems_[i] = prng_eng_();
-                }
+        for (int i = 0; i < nr_elems_; ++i) {
+            int to_put = to_put_eng_() % nr_elems_;
+            if (to_put < treshold) { // put in a number
+                size_t row = i / width_;
+                size_t col = i % width_;
+                elems_.push_back({static_cast<int>(prng_eng_()), row, col});
             }
-            break;
-        }
-        case dense: {
-            for (int i = 0; i < nr_elems_; ++i) {
-                elems_[i] = prng_eng_();
-            }
-            break;
-        }
         }
     }
 };
 
 int main()
 {
-    Matrix<int> mat(1000, Matrix<int>::sparse);
+    SparseMatrix<int> mat(10000);
 
     std::ofstream ofs("./mat.out");
-    ofs << mat;
+    ofs << mat << std::endl;
 
     return 0;
 }
