9e0df85ffa241bdc0bb33a76a651fd11eb6733ed
aregmk6 <areg631@gmail.com>
2026-05-26 00:15:17 +0300

    create naive generator for symmetric tiles

--- instruction-set/instgen.cpp
+++ instruction-set/instgen.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <limits>
 #include <random>
@@ -12,12 +13,6 @@
 
 using uint = unsigned int;
 
-struct Mat {
-    uint width;
-    uint height;
-    uint elem_width;
-};
-
 class InstGen
 {
   public:
@@ -27,21 +22,22 @@ class InstGen
 
     static constexpr size_t page_size = 4 * 1024;
 
-    InstGen(Mat mat_a, Mat mat_b)
+    InstGen(uint a_width, uint a_height, uint a_elem_width, uint b_width,
+            uint b_height, uint b_elem_width)
     {
         using long_limit = std::numeric_limits<Addr>;
 
-        if (mat_a.width != mat_b.height) {
+        if (a_width != b_height) {
             std::cerr << "invalid matrix dimantions for multiplication"
                       << std::endl;
             throw;
         }
 
-        size_t a_byte_size = mat_a.width * mat_a.height * mat_a.elem_width;
-        size_t b_byte_size = mat_b.width * mat_b.height * mat_b.elem_width;
+        size_t a_byte_size = a_width * a_height * a_elem_width;
+        size_t b_byte_size = b_width * b_height * b_elem_width;
 
-        ElemWidth c_elem_width = std::max(mat_a.elem_width, mat_b.elem_width);
-        size_t c_byte_size     = mat_a.height * mat_b.width * c_elem_width;
+        ElemWidth c_elem_width = std::max(a_elem_width, b_elem_width);
+        size_t c_byte_size     = a_height * b_width * c_elem_width;
 
         // generate A address and make sure it leaves enough space for B
         std::mt19937_64 prng_addr_{std::random_device()()};
@@ -75,52 +71,98 @@ class InstGen
 
         assert(c_addr - b_addr >= b_byte_size);
 
-        A = {mat_a.width, mat_a.height, mat_a.elem_width, a_addr};
-        B = {mat_b.width, mat_b.height, mat_b.elem_width, b_addr};
-        C = {mat_a.height, mat_b.width, c_elem_width, c_addr};
+        A = GhostMat{a_width, a_height, a_elem_width, a_addr};
+        B = GhostMat{b_width, b_height, b_elem_width, b_addr};
+        C = GhostMat{a_height, b_width, c_elem_width, c_addr};
     }
 
     // LOAD_TILE <Tile ID><Base Addr><Width><Height><Stride><Element Size>
 
-    // loading matrix A, from AB=C. The left matrix, so we need to shift the
-    // tiles to the right, by using their width.
-    void load_left() const
+    // TODO: support a-sym tiles
+
+    // k is the tile width ratio
+    void generate(int k, std::ostream& os) const
     {
-        printf("LOAD_TILE %d, 0x%lX, %d, %d, %d, %d\n", 0, A.addr, A.width / 2,
-               A.height / 2, A.width, A.elem_width);
-    }
+        static constexpr uint a_id = 0;
+        static constexpr uint b_id = 1;
+        static constexpr uint c_id = 2;
 
-    // loading matrix B, from AB=C. The right matrix, so we need to shift the
-    // tiles down, by using their height.
-    void load_right() const
-    {
-        printf("LOAD_TILE %d, 0x%lX, %d, %d, %d, %d\n", 1, B.addr, B.width / 2,
-               B.height / 2, B.width, B.elem_width);
-    }
+        int tile_width  = C.width / k;
+        int tile_height = tile_width;
 
-    void store() const;
+        int tci = 0;
+        int tri = 0;
 
-    void mul_acc() const;
+        while (tri * C.width * tile_height + tci * tile_width <
+               C.total_elem_size) {
 
-    void generate() const
-    {
-        constexpr int tile_width = 10;
+            Addr c_disp = tri * C.width * tile_height * C.elem_width;
 
-        while (1) {
-            load_left();
-            load_right();
+            os << "LOAD_TILE" << ' ' << c_id << ", " << "0x" << std::hex //
+               << C.addr + c_disp + tci * tile_width * C.elem_width      //
+               << std::dec << ", " << tile_width << ", "                 //
+               << tile_height << ", " << C.width << ", "                 //
+               << C.elem_width << std::endl;                             //
 
-            mul_acc();
+            int in_tci = 0;
+            int in_tri = 0;
+
+            while (in_tci * tile_width < A.width) {
+                Addr a_disp = tri * A.width * tile_height * A.elem_width;
+                Addr b_disp = tci * tile_width * B.elem_width;
+
+                os << "LOAD_TILE" << ' ' << a_id << ", " << "0x" << std::hex //
+                   << A.addr + a_disp + in_tci * tile_width * A.elem_width   //
+                   << std::dec << ", " << tile_width << ", "                 //
+                   << tile_height << ", " << A.width << ", "                 //
+                   << A.elem_width << std::endl;                             //
+
+                os << "LOAD_TILE" << ' ' << b_id << ", " << "0x" << std::hex //
+                   << B.addr + b_disp +                                      //
+                          in_tri * B.width * tile_height * B.elem_width      //
+                   << std::dec << ", " << tile_width << ", "                 //
+                   << tile_height << ", " << B.width << ", "                 //
+                   << B.elem_width << std::endl;                             //
+
+                os << "TILE_MUL_ACC" << ' ' << a_id << ", " << b_id << ", "
+                   << c_id << std::endl;
+
+                ++in_tci;
+                ++in_tri;
+            }
+
+            os << "STORE_TILE " << ' ' << c_id << ", " << "0x" << std::hex //
+               << C.addr + tri * C.width * C.elem_width +                  //
+                      tci * tile_width * C.elem_width                      //
+               << std::dec << ", " << tile_width << ", "                   //
+               << tile_height << ", " << C.width << ", "                   //
+               << C.elem_width << std::endl;                               //
+
+            ++tci;
+            if (tci * tile_width >= C.width) {
+                tci = 0;
+                ++tri;
+            }
         }
-
-        store();
     }
 
   private:
     struct GhostMat {
+        GhostMat() = default;
+
+        GhostMat(uint w, uint h, uint elem_w, Addr a)
+            : width(w), height(h), elem_width(elem_w),
+              total_elem_size(width * height),
+              total_byte_size(width * height * elem_width), addr(a)
+        {
+        }
+
         uint width;
         uint height;
         uint elem_width;
+
+        size_t total_elem_size;
+        size_t total_byte_size;
 
         Addr addr;
     };
@@ -132,9 +174,15 @@ class InstGen
 
 int main()
 {
-    InstGen gen{{100, 100, 64}, {100, 100, 8}};
-    gen.load_left();
-    gen.load_right();
+    InstGen gen{100, 100, 64, 100, 100, 8};
+
+    std::ofstream ofs("matmul.matv");
+
+    if (!ofs.is_open()) {
+        std::cerr << "error opening file" << std::endl;
+    }
+
+    gen.generate(4, ofs);
 
     return 0;
 };
