1985cca8b0cdf2ae4ff4a19a2f1541b34e411344
George (Gosha) Ginzburg <ginzburggeorgy@gmail.com>
2026-05-31 14:20:17 +0300

    add memory system

---
+++ memory-system/memory_object.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstddef>
+
+class MemoryObject
+{
+  public:
+    using Addr   = unsigned long;
+    using Cycles = unsigned long;
+
+    explicit MemoryObject(Cycles access_cycles, MemoryObject* next) : access_cycles_(access_cycles), next_(next)
+    {
+    }
+
+    virtual ~MemoryObject() = default;
+
+    Cycles accessCycles() const { return access_cycles_; }
+
+    virtual Cycles read(Addr addr, size_t size)  = 0;
+    virtual Cycles write(Addr addr, size_t size) = 0;
+
+  protected:
+    Cycles access_cycles_;
+    MemoryObject* next_;
+};
