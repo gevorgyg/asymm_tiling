d3c84894e22aac3a2a31305675241aeebcff3331
aregmk6 <areg631@gmail.com>
2026-09-25 23:16:47 +0300

    start implementing CLI11 for input parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,47 @@
+#include "CLI/CLI.hpp"
 #include "matrix_factory.h"
 #include "multi_unit.h"
 #include "registry.h"
 
+#include <CLI/CLI.hpp>
 #include <fmt/format.h>
-#include <spdlog/async.h>
-#include <spdlog/sinks/ansicolor_sink.h>
+#include <map>
 #include <spdlog/spdlog.h>
+
+using BSource = MultiUnit::BSource;
+
+struct MyOptions : public CLI::App {
+    using super = CLI::App;
+
+    using BSource = MultiUnit::BSource;
+
+    BSource b_source = BSource::memory;
+
+    const std::map<std::string, BSource> b_source_map{
+        {"memory", MultiUnit::BSource::memory},
+        {"mem", MultiUnit::BSource::memory},
+        {"m", MultiUnit::BSource::memory},
+        {"fifo", MultiUnit::BSource::fifo},
+        {"f", MultiUnit::BSource::fifo},
+    };
+
+    explicit MyOptions(std::string app_description = "",
+                       std::string app_name        = "")
+        : App(app_description, app_name)
+    {
+        add_option("-B, --BSource", b_source,
+                   "Choose B elemnt source between memory or prng fifo")
+            ->transform(
+                CLI::CheckedTransformer(b_source_map, CLI::ignore_case));
+    }
+};
 
 int main(int argc, char* argv[])
 {
-    // init logger
-    spdlog::init_thread_pool(8192, 1);
-    std::shared_ptr<spdlog::logger> bg_console_logger =
-        spdlog::create_async<spdlog::sinks::ansicolor_stdout_sink_mt>(
-            "b_console_logger");
-    spdlog::set_default_logger(bg_console_logger);
-    spdlog::info("Initialized Async Logger");
+    spdlog::info("Initialized Logger");
 
     // create matrices
-    MatrixFactory mat_factory{100, 100, 100, 1, 4};
+    MatrixFactory mat_factory{1000, 1000, 1000, 1, 4};
     const size_t tile_w = 25, tile_h = 25;
 
     MultiUnit m{
