fa44276157ff523e479aeb34140dbe07088e6316
aregmk6 <areg631@gmail.com>
2026-09-26 19:44:39 +0300

    finish plugging options with main

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,10 @@ using BSource = MultiUnit::BSource;
 struct MyOptions : public CLI::App {
     using super   = CLI::App;
     using BSource = MultiUnit::BSource;
+    enum oriantation { output, weight };
+
+    // multiplication options
+    oriantation mult_oriantation = weight;
 
     // matrix factory options
     uint32_t m               = 100;
@@ -34,10 +38,28 @@ struct MyOptions : public CLI::App {
     size_t tile_w{n / 2};
     BSource b_source = BSource::memory;
 
+    // CacheUnit{6, 100, 14, 4, 3, 16, 20, 3, true};
+
+    // cache unit options
+    int block_size = 6;
+    int mem_cycles = 100;
+    int l1_size    = 14;
+    int l1_cycles  = 4;
+    int l1_assoc   = 3;
+    int l2_size    = 16;
+    int l2_cycles  = 20;
+    int l2_assoc   = 3;
+
+    bool write_alloc = false;
+
     explicit MyOptions(std::string app_description = "",
                        std::string app_name        = "")
         : App(app_description, app_name)
     {
+        add_option("-o, --oriantation", mult_oriantation,
+                   "choose between output stationary, or weight stationary")
+            ->transform(
+                CLI::CheckedTransformer(oriantation_map, CLI::ignore_case));
 
         add_option("-m, --matrix-height", m, "choose matrix height dimantion");
         add_option("-k, --inner-dimantion", k, "choose inner dimantion");
@@ -111,9 +133,28 @@ struct MyOptions : public CLI::App {
                    "Choose B elemnt source between memory or prng fifo")
             ->transform(
                 CLI::CheckedTransformer(b_source_map, CLI::ignore_case));
+
+        add_option("-c, --cache", cache_options,
+                   "set cache options in units of log2 [ block_size, "
+                   "mem_cycles, l1_size, l1_cycles, "
+                   "l1_assoc, l2_size, l2_cycles, l2_assoc ]");
+
+        auto [block_size, mem_cycles, l1_size, l1_cycles, l1_assoc, l2_size,
+              l2_cycles, l2_assoc] = cache_options;
+
+        add_flag("-w, --write-allocate, --no-write-allocate{false}",
+                 write_alloc, "set write allocate");
     }
 
   private:
+    std::array<int, 8> cache_options;
+
+    const std::map<std::string, oriantation> oriantation_map{
+        {"output", output}, {"out", output}, {"o", output},
+        {"C", output},      {"c", output},   {"weight", weight},
+        {"w", weight},      {"B", weight},   {"b", weight},
+    };
+
     const std::map<std::string, BSource> b_source_map{
         {"memory", MultiUnit::BSource::memory},
         {"mem", MultiUnit::BSource::memory},
@@ -125,26 +166,34 @@ struct MyOptions : public CLI::App {
 
 int main(int argc, char* argv[])
 {
-    MyOptions parser{"asymm tiling options", "asymm tiling"};
-    parser.parse(argc, argv);
+    MyOptions options{"asymm tiling options", "asymm tiling"};
+    CLI11_PARSE(options, argc, argv);
 
-    spdlog::info("Initialized Logger");
+    spdlog::info("Asymm Matrix Multiplication Log Start");
 
     // create matrices
-    MatrixFactory mat_factory{100, 100, 100, 1, 4};
+    MatrixFactory mat_factory{options.m, options.k, options.n,
+                              options.small_percision, options.ratio};
     const size_t tile_w = 25, tile_h = 25;
 
     MultiUnit m{
-        CacheUnit{6, 100, 14, 4, 3, 16, 20, 3, true},
-        PrngFifo{14, 10, 2, 8},
+        CacheUnit{options.block_size, options.mem_cycles, options.l1_size,
+                  options.l1_cycles, options.l1_assoc, options.l2_size,
+                  options.l2_cycles, options.l2_assoc, options.write_alloc},
+        PrngFifo{options.capacity, options.generation_cost,
+                 options.accsess_cost, options.seed_size},
         mat_factory.create_mats(),
-        tile_w,
-        tile_h,
-        MultiUnit::fifo,
+        options.tile_w,
+        options.tile_h,
+        options.b_source,
     };
 
     // start simulation
-    m.weight_stat_matmul();
+    if (options.mult_oriantation) {
+        m.output_stat_matmul();
+    } else {
+        m.weight_stat_matmul();
+    }
 
     gRegistry().dump();
 
